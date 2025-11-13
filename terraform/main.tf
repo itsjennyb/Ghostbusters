cb58696017b2a9fa8c5f69723f86d7a6a17f5bb0
Jennifer Blacutt <jblac@mac.attlocal.net>
2025-11-13 12:22:19 -0600

    rename iam role

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,7 +66,7 @@ resource "aws_s3_object" "frontend_assets" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
+  name = "lambda_exec_role-${var.environment}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
