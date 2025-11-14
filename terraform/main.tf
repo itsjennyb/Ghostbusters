2b3fc84546aa72935875ae3d6398cfc9a70e83a3
Jennifer Blacutt <jblac@Mac.attlocal.net>
2025-11-14 14:15:54 -0600

    fix cors headers issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,6 +104,15 @@ resource "aws_lambda_function" "backend" {
 resource "aws_apigatewayv2_api" "api" {
   name          = "ghostbusters-lambda-API-${var.environment}"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins     = ["https://di1k0hz1g0u8v.cloudfront.net"]
+    allow_methods     = ["POST", "OPTIONS"]
+    allow_headers     = ["content-type", "authorization"]
+    expose_headers    = []
+    max_age           = 3600
+    allow_credentials = false
+  }
 }
 
 resource "aws_apigatewayv2_stage" "default" {
@@ -285,5 +294,57 @@ resource "aws_dynamodb_table" "users" {
 
   tags = {
     Environment = var.environment
+  }
+}
+
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.backend.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options" {
+  rest_api_id = aws_api_gateway_method.options.rest_api_id
+  resource_id = aws_api_gateway_method.options.resource_id
+  http_method = aws_api_gateway_method.options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options" {
+  rest_api_id = aws_api_gateway_method.options.rest_api_id
+  resource_id = aws_api_gateway_method.options.resource_id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options" {
+  rest_api_id = aws_api_gateway_method.options.rest_api_id
+  resource_id = aws_api_gateway_method.options.resource_id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = aws_api_gateway_method_response.options.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'https://di1k0hz1g0u8v.cloudfront.net'"
+  }
+
+  response_templates = {
+    "application/json" = ""
   }
 }
